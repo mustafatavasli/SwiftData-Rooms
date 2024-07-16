df24ae36b57fa497df04e36b182ce94c79cfd6e2
mustafatavasli <musstafatavasli@gmail.com>
2024-07-16 11:56:27 +0300

    Initial Commit

---
+++ Rooms/RoomsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomsApp.swift
+//  Rooms
+//
+//  Created by Mustafa TAVASLI on 16.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct RoomsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
